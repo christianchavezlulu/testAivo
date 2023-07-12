3122fced74c41e542991b8025b2d1cdefc393521
christianchavezlulu <cchavez2@lululemon.com>
2023-07-12 13:47:31 -0500

    add a filter in HomePage component

--- src/Home/HomePage.jsx
+++ src/Home/HomePage.jsx
@@ -5,6 +5,7 @@ import Profile from '../Profile/Profile';
   const HomePage = () => {
   const [movies, setMovies] = useState>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [filter, setFilter] = useState('all');
 
   useEffect(() => {
     fetchData();
@@ -21,19 +22,44 @@ import Profile from '../Profile/Profile';
     }
   };
 
+  const filteredMovies = movies.filter((movie) => {
+    if (filter === 'all') {
+      return true;
+    } else if (filter === 'movie') {
+      return movie.programType === 'movie';
+    } else if (filter === 'series') {
+      return movie.programType === 'series';
+    }
+    return true;
+  });
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
     <div>
-      <h1>All the Movies</h1>
+      <h1>The Movies</h1>
       <Profile />
+      <div>
+        <label>
+          Filter by:
+          <select value={filter} onChange={handleFilterChange}>
+            <option value="all">All</option>
+            <option value="movie">Movie</option>
+            <option value="series">Series</option>
+          </select>
+        </label>
+      </div>
       <span>
-        Total: {movies.length}
+        Total: {filteredMovies.length}
       </span>
       <ul>
-        {movies.map(movie => (
+        {filteredMovies.map(movie => (
           <Movie key={movie.title} movie={movie} />
         ))}
       </ul>
