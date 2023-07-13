39b6e69ed4e716021d433dced6474d97fae96a36
christianchavezlulu <cchavez2@lululemon.com>
2023-07-13 17:09:14 -0500

    add style with materialUi to Movie component

--- src/Movie/Movie.jsx
+++ src/Movie/Movie.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import { flexbox } from '@mui/system';
+
+const useStyles = makeStyles({
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    maxWidth: 200,
+    margin: '0 auto',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    marginBottom: '16px',
+    padding: '10px'
+  },
+  media: {
+    height: 150,
+    width: 150,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    marginTop: '8px',
+    fontWeight: 'bold',
+    fontSize: '18px',
+    color: '#333'
+  },
+});
 
 const Movie = ({ movie: { title='Without title', images={} } }) => {
+  const classes = useStyles();
+
   // Check if the images object and the 'Poster Art' property are defined
   if (!images || !images['Poster Art']) {
     return null; // If not, return null or a placeholder component
@@ -10,10 +45,10 @@ const Movie = ({ movie: { title='Without title', images={} } }) => {
   const { url } = images['Poster Art'];
 
   return (
-    <div>
-        <h2>{title}</h2>
-        <img src={url} alt={title} />
-    </div>
+    <Card className={classes.card}>
+      <CardMedia className={classes.media} image={url} title={title} />
+      <Typography className={classes.title} variant="h6">{title}</Typography>
+    </Card>
   )
 }
 
